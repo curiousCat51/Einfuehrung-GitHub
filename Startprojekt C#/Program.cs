1453c0c7c1be1f55474fbbe1e2e45eba4dc9c558
curiousCat51 <jfleer@hansalog.de>
2026-08-21 10:48:35 +0200

    C# Projekt Update

--- Startprojekt C#/Program.cs
+++ Startprojekt C#/Program.cs
@@ -1,6 +1,6 @@
-﻿#Beginnt bei 1, zählt in jedem Durchlauf 1 hoch bis die Zahl 10 erreicht ist.
+﻿//Beginnt bei 1, zählt in jedem Durchlauf 1 hoch bis die Zahl 10 erreicht ist.
 for(int i = 1; i <= 10; i++)
 {
-    #Gibt die aktuelle Zahl in der Konsole aus.
+    //Gibt die aktuelle Zahl in der Konsole aus.
     Console.WriteLine($"Number: {i}");
 }
